c946d02ca395d121567150e669fabeb73c27d42f
Vera Malieske <vera.malieske@web.de>
2025-12-18 20:29:06 +0100

    Extract selectionBar
    - extract selectionBar in selection-container-component
    - menu-options should be left in the specific component and be projected in the selectionBar

--- src/app/components/selection/selection-container.component.ts
+++ src/app/components/selection/selection-container.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, TemplateRef, Input } from '@angular/core';
 import { IEntity } from 'src/common';
 import { SelectionService } from 'src/app/services/selection.service';
 import { SelectionBox } from 'src/app/pages/profile-page/selection-box/selection-box.component';
+import { TranslatePipe } from '../../pipes/translate.pipe';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-selection-container',
@@ -10,9 +13,10 @@ import { SelectionBox } from 'src/app/pages/profile-page/selection-box/selection
   styleUrls: ['./selection-container.component.scss'],
   standalone: true,
   providers: [SelectionService],
-  imports: [CommonModule, SelectionBox],
+  imports: [CommonModule, SelectionBox, TranslatePipe, MatIconModule, MatButtonModule],
 })
 export class SelectionContainerComponent {
+  @Input() actionsTemplate!: TemplateRef<unknown>;
   public selectionService = inject(SelectionService);
 
   public selectionServiceSignal = computed(() => this.selectionService);

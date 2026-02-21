f891eb1b17ffed24ec6b13d4f9b5cd5b22287703
Vera Malieske <vera.malieske@web.de>
2026-02-21 16:16:28 +0100

    Metadata-Update Part 1
    Regarding Issue #159
    
    General Decisions:
    - The icon for an option tab is the same now
    - We removed "Other" as optional metadata
    - "Physical object" becomes "Original object"
    - Remove Statement, Object type and Discipline
    
    General information:
    - Add examples for tags, so the removed fields could be replaced with a tag by the user

--- src/app/components/entity-detail/detail-entity/detail-entity.component.ts
+++ src/app/components/entity-detail/detail-entity/detail-entity.component.ts
@@ -53,15 +53,6 @@ export class DetailEntityComponent {
     return this.metadataFiles().length > 0;
   });
 
-  otherMetadata = computed(() => {
-    const entity = this.entity();
-    return entity.other;
-  });
-
-  hasOtherMetadata = computed(() => {
-    return this.otherMetadata().length > 0;
-  });
-
   bibRefs = computed(() => {
     const entity = this.entity();
     return entity.biblioRefs;
